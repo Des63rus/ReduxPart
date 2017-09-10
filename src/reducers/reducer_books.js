36e6cd4e5b186b687eef166e464df9f3dab94758
Andrei Kolesnikov <andreik@il.ibm.com>
2017-09-10 18:19:05 +0300

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function (){
+    return[
+        {title: 'Harry Potter'},
+        {title: 'Javascript: the Good Parts'},
+        {title: 'The Dark Tower'},
+        {title: 'Eloquent Ruby'}
+    ];
+}
